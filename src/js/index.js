f248594b18ad3f7d72f221bd91e7e8164907226b
SetHet <36393893+SetHet@users.noreply.github.com>
2023-02-03 14:08:13 -0300

    Toggle Play

--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,20 @@ MediaPlayer.prototype.play = function() {
     this.media.play();
 }
 
+MediaPlayer.prototype.pause = function() {
+    this.media.pause()
+}
+
+MediaPlayer.prototype.togglePlay = function() {
+    if (this.media.paused){
+        this.play();
+    } else {
+        this.pause();
+    }
+}
+
 const player = new MediaPlayer({
     el: video
 });
 
-button.onclick = () => player.play();
+button.onclick = () => player.togglePlay();
